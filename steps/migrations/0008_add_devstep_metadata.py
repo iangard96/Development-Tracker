0f5bafb209d5821e6d8aed7c76e53d30c62f2089
iangard96 <64283839+iangard96@users.noreply.github.com>
2025-12-07 19:58:07 -0600

    Update 0008_add_devstep_metadata.py

--- steps/migrations/0008_add_devstep_metadata.py
+++ steps/migrations/0008_add_devstep_metadata.py
@@ -1,3 +1,4 @@
+# steps/migrations/0008_add_devstep_metadata.py
 from django.db import migrations
 
 
@@ -6,40 +7,5 @@ class Migration(migrations.Migration):
         ("steps", "0007_projectcontact"),
     ]
 
-    operations = [
-        migrations.RunSQL("CREATE SCHEMA IF NOT EXISTS app;"),
-        
-        migrations.RunSQL(
-            """
-            ALTER TABLE app."DevTracker"
-            ADD COLUMN IF NOT EXISTS agency varchar(255);
-
-            ALTER TABLE app."DevTracker"
-            ADD COLUMN IF NOT EXISTS responsible_party varchar(255);
-
-            ALTER TABLE app."DevTracker"
-            ADD COLUMN IF NOT EXISTS responsible_individual varchar(255);
-
-            ALTER TABLE app."DevTracker"
-            ADD COLUMN IF NOT EXISTS process text;
-
-            ALTER TABLE app."DevTracker"
-            ADD COLUMN IF NOT EXISTS link text;
-
-            ALTER TABLE app."DevTracker"
-            ADD COLUMN IF NOT EXISTS requirement text;
-
-            ALTER TABLE app."DevTracker"
-            ADD COLUMN IF NOT EXISTS purpose_related_activity integer;
-            """,
-            """
-            ALTER TABLE app."DevTracker" DROP COLUMN IF EXISTS purpose_related_activity;
-            ALTER TABLE app."DevTracker" DROP COLUMN IF EXISTS requirement;
-            ALTER TABLE app."DevTracker" DROP COLUMN IF EXISTS link;
-            ALTER TABLE app."DevTracker" DROP COLUMN IF EXISTS process;
-            ALTER TABLE app."DevTracker" DROP COLUMN IF EXISTS responsible_individual;
-            ALTER TABLE app."DevTracker" DROP COLUMN IF EXISTS responsible_party;
-            ALTER TABLE app."DevTracker" DROP COLUMN IF EXISTS agency;
-            """,
-        )
-    ]
+    # Make this migration a no-op on fresh databases (like Render)
+    operations = []
