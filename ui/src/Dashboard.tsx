0b1c2cd885cd3fe48eaa1aed138f64cbdd25760d
iangard96 <64283839+iangard96@users.noreply.github.com>
2025-12-10 23:19:15 -0600

    Import useMemo for permit matrix in dashboard

--- ui/src/Dashboard.tsx
+++ ui/src/Dashboard.tsx
@@ -1,5 +1,5 @@
 // ui/src/Dashboard.tsx
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { fetchStepsForProject } from "./api";
 import DevTypeProgressRow from "./DevTypeProgressRow";
 import DevTypeSpendChart from "./DevTypeSpendChart";
